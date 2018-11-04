99f80379ee5ff3b6dcda4205f954e5973732705d
Dominik Manowski <manowski@protonmail.com>
2018-11-04 12:10:31 +0100

    Add tasks

---
+++ src/scripts.js
@@ -0,0 +1,7 @@
+// Get all elements
+// Define variables
+// Get list word
+// Create underscores based on picked word length
+// Prepare board
+// Get and store value from the input
+// Check if guess is right
