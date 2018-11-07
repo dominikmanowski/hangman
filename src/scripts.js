6114935d1991f6999748d9a586c5a4ffd4b32150
Dominik Manowski <manowski@protonmail.com>
2018-11-07 16:27:21 +0100

    Add option of entering more than one letter

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,5 @@
 // Select elements
 const select = name => document.querySelector(`.game-board__${name}`);
-// const hangman = nr => document.querySelector(`.game-board__gallows-element--${nr}`);
 
 const categoryDisp = select("category-display");
 const wordToGuess = select("word-to-guess");
@@ -97,12 +96,10 @@ function newRound() {
   prepareBoard();
 })();
 
-// Get and store value from the input
-
 submitBtn.addEventListener("click", e => {
   e.preventDefault();
   if (input.value.length !== 0) {
-    currentGuess = input.value[0].toLowerCase();
+    currentGuess = input.value.toLowerCase().split('');
     input.value = "";
     checkGuess();
     input.focus();
@@ -112,33 +109,35 @@ submitBtn.addEventListener("click", e => {
 // Check if guess is right
 
 function checkGuess() {
-  if (word.includes(currentGuess)) {
-    word.forEach((letter, i) => {
-      if (letter === currentGuess && !rightLettersArr.includes(currentGuess)) {
-        lettersListElements[i].textContent = currentGuess.toUpperCase();
-        rightGuessCount++;
+  currentGuess.forEach(char => {
+    if (word.includes(char)) {
+      word.forEach((letter, i) => {
+        if (letter === char && !rightLettersArr.includes(char)) {
+          lettersListElements[i].textContent = char.toUpperCase();
+          rightGuessCount++;
+        }
+      });
+      rightLettersArr.push(char);
+    } else if (!wrongLettersArr.includes(char)) {
+      wrongLetters.textContent += ` ${char.toUpperCase()}`;
+      wrongLettersArr.push(char);
+      lives--;
+      drawHangman(lives)
+      livesDisp.textContent = lives;
+      if (lives === 0) {
+        setTimeout(() => {
+          if (confirm("You lost. Do you want to play again?")) {
+            newRound();
+          }
+        }, 200);
       }
-    });
-    rightLettersArr.push(currentGuess);
-  } else if (!wrongLettersArr.includes(currentGuess)) {
-    wrongLetters.textContent += ` ${currentGuess.toUpperCase()}`;
-    wrongLettersArr.push(currentGuess);
-    lives--;
-    drawHangman(lives)
-    livesDisp.textContent = lives;
-    if (lives === 0) {
+    }
+    if (rightGuessCount === word.length) {
       setTimeout(() => {
-        if (confirm("You lost. Do you want to play again?")) {
+        if (confirm("You won! Do you want to play again?")) {
           newRound();
         }
       }, 200);
     }
-  }
-  if (rightGuessCount === word.length) {
-    setTimeout(() => {
-      if (confirm("You won! Do you want to play again?")) {
-        newRound();
-      }
-    }, 200);
-  }
+  })
 }
